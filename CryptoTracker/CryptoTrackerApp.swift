c9ab2d6e6ef53a7f00ed43f7e5de2d95b338e757
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2022-10-15 15:31:14 +0530

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Dev C Krishna on 15/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
