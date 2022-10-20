426efd8917a86bc9734ae9642ce52415cd58030e
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2022-10-20 18:18:25 +0530

    Portfolio section done 🌺

--- CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -16,6 +16,7 @@ class HomeViewModel: ObservableObject {
     
     private let dataService = CoinDataService()
     private let marketDataService = MarketDataService()
+    private let portfolioDataService = PortfolioDataService()
     private var cancellabels = Set<AnyCancellable>()
     
     init() {
@@ -39,6 +40,25 @@ class HomeViewModel: ObservableObject {
                     }
                     .store(in: &cancellabels)
         
+        $allCoins
+            .combineLatest(portfolioDataService.$savedEntities)
+            .map { (coinModels, portfolioEntities) -> [CoinModel] in
+                coinModels
+                    .compactMap { (coin) -> CoinModel? in
+                        guard let entity = portfolioEntities.first(where: { $0.coinID == coin.id}) else{
+                            return nil
+                        }
+                        return coin.updateHoldings(amount: entity.amount)
+                    }
+            }
+            .sink { [weak self] (returnedCoins) in
+                self?.portfolioCoins  = returnedCoins
+            }
+            .store(in: &cancellabels)
+    }
+    
+    func updatePortfolio(coin: CoinModel, amount: Double) {
+        portfolioDataService.updatePortfolio(coin: coin, amount: amount)
     }
     
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
